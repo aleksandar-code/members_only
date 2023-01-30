6ab3163ec7d6bc18270db00111edbbc26b1447c5
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-01-30 22:57:00 +0100

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+  
   def index
+      @posts = Post.all
+  end
+  
+  def new
+      @post = Post.new
   end
 
-  def new
-  end
 
   def create
+      @user = User.new(user_params)
+
+      if @user.save
+      redirect_to root_path
+      else
+      render :new
+      end
+  end
+
+  private
+  
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :members
   root to: "posts#index"
+  resources :members
   resources :posts, only: [:new, :create, :index]
 end
